00eb1b2d755499cf4a56c3495e053f91124b3428
Prachan Ghale <ghale.prachan@gmail.com>
2020-12-19 20:37:27 +0545

    [Updated] updated more options

--- app/src/main/java/com/example/roofcare/helper/populateMoreSection/PopulateMoreOptions.java
+++ app/src/main/java/com/example/roofcare/helper/populateMoreSection/PopulateMoreOptions.java
@@ -7,8 +7,8 @@ import com.example.roofcare.models.moreOptionModel.MoreOptionsModel;
 
 public class PopulateMoreOptions {
     public static void addOptions() {
-        MoreOptionsModel modelOne = new MoreOptionsModel(MoreOptionsId.BOOKINGS, "Bookings", R.drawable.ic_baseline_book_24);
-        MoreOptionsModel modelTwo = new MoreOptionsModel(MoreOptionsId.BOOKING_REQUESTS, "Booking Requests", R.drawable.ic_baseline_post_add_24);
+        //MoreOptionsModel modelOne = new MoreOptionsModel(MoreOptionsId.BOOKINGS, "Bookings", R.drawable.ic_baseline_book_24);
+        MoreOptionsModel modelTwo = new MoreOptionsModel(MoreOptionsId.BOOKINGS, "Bookings", R.drawable.ic_baseline_post_add_24);
         MoreOptionsModel modelThree = new MoreOptionsModel(MoreOptionsId.BOOKING_HISTORY, "Booking History", R.drawable.ic_baseline_history_24);
         MoreOptionsModel modelFour = new MoreOptionsModel(MoreOptionsId.SETTINGS, "Settings", R.drawable.ic_baseline_settings_24);
         MoreOptionsModel modelFive = new MoreOptionsModel(MoreOptionsId.SAVED, "Saved", R.drawable.ic_baseline_bookmark_24);
@@ -16,7 +16,7 @@ public class PopulateMoreOptions {
         MoreOptionsModel modelSeven = new MoreOptionsModel(MoreOptionsId.SWITCH_ACCOUNT, "Switch Account", R.drawable.ic_baseline_switch_left_24);
         MoreOptionsModel modelEight = new MoreOptionsModel(MoreOptionsId.SIGN_OUT, "Sign Out", R.drawable.ic_baseline_power_settings_new_24);
 
-        MoreOptionsService.addOptions(modelOne);
+//        MoreOptionsService.addOptions(modelOne);
         MoreOptionsService.addOptions(modelTwo);
         MoreOptionsService.addOptions(modelThree);
         MoreOptionsService.addOptions(modelFour);
